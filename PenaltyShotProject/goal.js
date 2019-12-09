4281ab5d022888d77deb214004e6f7dbed363882
augiegallun <agallun21@nscds.org>
2019-12-09 19:08:32 +0000

    fixed up the goal/block if else statements

--- PenaltyShotProject/goal.js
+++ PenaltyShotProject/goal.js
@@ -1,28 +1,3 @@
-$('#upperleft').click(
-    function(){
-        TweenMax.to('#soccerball',.5,{x:-205, y:-530, delay:0.5, ease:Sine.easeOut});
-        TweenMax.to('#soccerball',.8,{x:-205, y:-310, delay:1, ease:Bounce.easeOut});
-    });
-    
-$('#lowerleft').click(
-    function(){
-        TweenMax.to('#soccerball',.2,{x:-205, y:-310, delay:0.5, ease:Sine.easeOut});
-        TweenMax.to('#soccerball',.4,{x:0, y:-330, delay:0.75, ease:Sine.easeOut});
-    });
-    
-$('#upperright').click(
-    function(){
-        TweenMax.to('#soccerball',.4,{x:205, y:-530, delay:0.4, ease:Sine.easeOut});
-        
-        if(distance(ballX, ballY, goalieX, goalieY) < 10){
-            TweenMax.to('#soccerball',.4,{x:240, y:-530, delay:0.5, ease:Sine.easeOut});
-        }
-        else{
-            TweenMax.to('#soccerball',.6,{x:205, y:-320, delay:0.8, ease:Bounce.easeOut});
-    }
-
-});
-
 function distance(x1, y1, x2, y2){
     var sideA = Math.abs(x1-x2);
     var sideB = Math.abs(y1-y2);
@@ -39,19 +14,36 @@ function outcome(blockX, blockY, goalX, goalY){
     var goalieX = goaliePosition.left;
     var goalieY = goaliePosition.top;
     
-    if(distance(ballX, ballY, goalieX, goalieY) < 100){
+    if(distance(ballX, ballY, goalieX, goalieY) < 300){
             TweenMax.to('#soccerball',.4,{x:blockX, y:blockY, delay:0.5, ease:Sine.easeOut});
+            console.log("block");
         }
         else{
-            TweenMax.to('#soccerball',.3,{x:goalX, y:goalY, delay:0.8, ease:Sine.easeOut});
+            TweenMax.to('#soccerball',.3,{x:goalX, y:goalY, delay:0.3, ease:Sine.easeOut});
+            console.log("goal");
+            
         }
 };
 
 $('#lowerright').click(
     function(){
-        TweenMax.to('#soccerball',.4,{x:220, y:-320, delay:0.5, ease:Sine.easeOut, onComplete:outcome, onCompleteParams:[blockX:250,blockY:-400,goalX:230,goalY:-350]});
-
+        TweenMax.to('#soccerball',.4,{x:220, y:-320, delay:0.5, ease:Sine.easeOut, onComplete:outcome, onCompleteParams:[400, -400, 100, -300]});
     });
+
+$('#upperleft').click(
+    function(){
+        TweenMax.to('#soccerball',.5,{x:-205, y:-530, delay:0.5, ease:Sine.easeOut, onComplete:outcome, onCompleteParams:[-400, -310, -205, -310]});
+    });
+    
+$('#lowerleft').click(
+    function(){
+        TweenMax.to('#soccerball',.2,{x:-205, y:-310, delay:0.5, ease:Sine.easeOut, onComplete:outcome, onCompleteParams:[-325, -350, -50, -340]});
+    });
+    
+$('#upperright').click(
+    function(){
+        TweenMax.to('#soccerball',.4,{x:205, y:-530, delay:0.4, ease:Sine.easeOut, onComplete:outcome, onCompleteParams:[-240, -530, 205, -320]});
+});
 
 
 
