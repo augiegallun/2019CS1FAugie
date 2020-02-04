cb0bbda5e13536ce51c0925d7f89a710fac3fd45
augiegallun <agallun21@nscds.org>
2020-02-04 19:21:05 +0000

    morse code

--- Semester 2/project 1/bruh.js
+++ Semester 2/project 1/bruh.js
@@ -1,6 +1,62 @@
+
 function morseCode(message) {
     var string = "";
-    var alphabet = {
+    var alphabet = [
+        {letter:"a",morse:".-"},
+        {letter:"b",morse:"-..."},
+        {letter:"c",morse:"-.-."},
+        {letter:"d",morse:"-.."},
+        {letter:"e",morse:"."},
+        {letter:"f",morse:"..-."},
+        {letter:"g",morse:"--."},
+        {letter:"h",morse:"...."},
+        {letter:"i",morse:".."},
+        {letter:"j",morse:".---"},
+        {letter:"k",morse:"-.-"},
+        {letter:"l",morse:".-.."},
+        {letter:"m",morse:"--"},
+        {letter:"n",morse:"-."},
+        {letter:"o",morse:"---"},
+        {letter:"p",morse:".--."},
+        {letter:"q",morse:"--.-"},
+        {letter:"r",morse:".-."},
+        {letter:"s",morse:"..."},
+        {letter:"t",morse:"-"},
+        {letter:"u",morse:"..-"},
+        {letter:"v",morse:"...-"},
+        {letter:"w",morse:".--"},
+        {letter:"x",morse:"-..-"},
+        {letter:"y",morse:"-.--"},
+        {letter:"z",morse:"--.."}
+    ];
+    
+    console.log(alphabet[25].letter);
+    console.log(alphabet[0].morse);
+    
+function checkLetter(letterObject) {
+    var needle = alphabet.findIndex(letterObject)
+    return letterObject.letter == needle;
+};
+
+function myFunction() {
+    alphabet[needle].morse;
+}
+
+console.log(myfunction(message));
+message = "a"
+   
+ //for(var i=0; i<message.length; i++){
+        //alphabet.charAt(i)
+    //}
+    //return string
+    
+//}
+//console.log(morseCode("hello bro"));
+
+/*$(document).keydown(function(event) { 
+
+});*/
+/*var alphabet = {
         "a" : ".-",
         "b" : "-...",
         "c" : "-.-.",
@@ -28,13 +84,4 @@ function morseCode(message) {
         "y" : "-.--",
         "z" : "--..",
         " " : "/",
-    };
-    for(var i=0; i=message.length; i++){
-        alphabet.
-    }
-    
-}
-
-/*$(document).keydown(function(event) { 
-
-});*/
+    };*/}
